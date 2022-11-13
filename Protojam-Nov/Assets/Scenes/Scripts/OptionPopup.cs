00ec643766c84b49f74ebaa94d96c03d7c929fca
KyteRog <117752830+KyteRog@users.noreply.github.com>
2022-11-13 21:08:17 +0900

    new main
    commit to new main

--- Protojam-Nov/Assets/Scenes/Scripts/OptionPopup.cs
+++ Protojam-Nov/Assets/Scenes/Scripts/OptionPopup.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionPopup : MonoBehaviour
 {
+    GameObject optionPopup;
+    private void Awake()
+    {
+        optionPopup = GameObject.Find("OptionPopupImage");
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        optionPopup.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void popup_on()
+    {
+        optionPopup.SetActive(true);
+    }
+    public void popup_off()
+    {
+        optionPopup.SetActive(false);
     }
 }
